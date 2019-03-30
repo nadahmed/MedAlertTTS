d740727c14b3e21501e9270df07a2e065036fec8
Electro Mysterio <raydiati0n@hotmail.com>
2019-03-30 13:32:49 +0600

    made it faster

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ import schedule
 import pyrebase
 import secrets
 from pygame import mixer
+import subprocess
 
 medicine={}
 
@@ -15,7 +16,7 @@ def stream_handler(message):
     if message['event']=='patch':
         for k in message['data']:
             medicine[path][k]= message['data'][k]
-        text ='আচ্ছা।'
+        text =u'আচ্ছা।'
         speak(text)
     if message['event']=='put':
         for key in message['data']:
@@ -23,10 +24,10 @@ def stream_handler(message):
                 medicine = message['data']
             else:
                 medicine[path] = message['data']
-        text = 'আমি এখন up to date!'
+        text = u'আমি এখন up to date!'
         speak(text)
     # print(medicine.encode('utf-8'))
-    print(medicine)
+    # print(medicine)
     scheduler(medicine)
     print('\n')
         
@@ -34,8 +35,10 @@ def stream_handler(message):
 def getData(email,password):
     print('INITIALIZING FIREBASE')
     firebase = pyrebase.initialize_app(secrets.config)
+    print('Signing up')
     user = firebase.auth().sign_in_with_email_and_password(email,password)
     db = firebase.database()
+    print('Fetching database')
     ref = db.child('users', user['localId'], 'medicines')
     ref.stream(stream_handler)
 
@@ -47,17 +50,26 @@ def wait(mixer):
         
 def speak(text):
 
-    print('\nTrying to speak\n')
-
+    
+    with open('file.txt', 'w+', encoding="utf-8") as f:
+        f.write(text)
     mixer.init()
-    with open('temp.mp3', 'wb') as f:
-        while 1:
-            try:
-                tts = gTTS(text=text, lang='bn')
-                tts.write_to_fp(f)
+    i=0
+    while 1:
+        try:
+            print('Fetching from TTS & Saving')
+            subprocess.run("gtts-cli --file file.txt --lang bn --output temp.mp3", shell=True )
+            # tts = gTTS(text=text, lang='bn')
+            # print('saving')
+            # tts.save('temp.mp3')
+            break
+        except Exception as e:
+            i+=1
+            print(e)
+            if i==3:
                 break
-            except:
-                continue
+            continue
+    print('\nTrying to speak\n')
     mp3 = mixer.music.load('temp.mp3')
     mixer.music.play()
     wait(mixer)
@@ -88,14 +100,14 @@ def scheduler(meds):
                 schedule.every().day.at(t['time']).do(speak, text = text)
 
 def sch_textmaker(name , myTime, notes):
-    text= "আপনার ঔষধ খাবার সময় হয়েছে।"
-    text+= 'ঔষধ এর নাম ' + name + '।'
+    text= u'আপনার ঔষধ খাবার সময় হয়েছে।'
+    text+= u'ঔষধ এর নাম ' + name + '।'
     if myTime:
-        text+='ঔষধ টি খাবেন খালি পেট এ।'
+        text+=u'ঔষধ টি খাবেন খালি পেট এ।'
     else:
-        text+='ঔষধ খাবার আগে কিছু খেয়ে নিবেন।'
+        text+=u'ঔষধ খাবার আগে কিছু খেয়ে নিবেন।'
     if notes != '':
-        text+= ' এবং সাথে আপনার কিছু নোট। যা হচ্চে, '+ notes + '।'
+        text+= u' এবং সাথে আপনার কিছু নোট। যা হচ্চে, '+ notes + u'।'
     return text
     # translator = Translator()
     # text = translator.translate(text, dest='bn').text
@@ -110,3 +122,4 @@ if __name__ == "__main__":
     # asyncio.run(main())
         
     
+
